682f9c0482ae2cdc3b05ce1d6371cde4a11ea16c
maxmfernan <maxmfernan@gmail.com>
2017-11-13 16:44:15 -0200

    Names are appearing in select forms

--- Codes/project_site/djvs/views.py
+++ Codes/project_site/djvs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.template import Context
+from .process_code import import_data
+
 
 def index_render(request):
 
-	return render(request, "index.html")
+	return render(request, "index.html",{"field_names": import_data.getFieldNames()} )
+
