1ba8fcbff57f4fb89994ff7226996586fd0af8fc
maxmfernan <maxmfernan@gmail.com>
2017-11-14 11:10:35 -0200

    Getting POST input. Only data test.

--- Codes/project_site/djvs/views.py
+++ Codes/project_site/djvs/views.py
@@ -4,6 +4,12 @@ from .process_code import import_data
 
 
 def index_render(request):
+	fields_to_read = []
+
+	if "op" in request.POST:
+		# DEBUG
+		#print("DEBUG >> " + " ".join( request.POST.getlist("op") ) )
+		fields_to_read = request.POST.getlist("op")
 
 	return render(request, "index.html",{"field_names": import_data.getFieldNames()} )
 
